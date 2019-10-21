60222116b7984af33768ee693037268f01eb2eb0
agrozyme <agrozyme@gmail.com>
2019-10-21 19:37:27 +0800

    update

--- rootfs/usr/local/bin/docker-build.lua
+++ rootfs/usr/local/bin/docker-build.lua
@@ -6,7 +6,11 @@ local function main()
   local www = "/var/www"
   local project = "/tmp/drupal-project"
   core.run("apk add --no-cache lua-rex-pcre")
-  core.run("%s/composer create-project --no-install drupal-composer/drupal-project:8.x-dev %s", bin, project)
+  core.run(
+    "%s/composer create-project --no-install --no-interaction drupal-composer/drupal-project:8.x-dev %s",
+    bin,
+    project
+  )
   core.run("mv %s/composer.json %s/composer.json", www, project)
   core.run("%s/composer --working-dir=%s install", bin, project)
   core.run("tar -czf %s/drupal-project.tgz -C %s .", www, project)
